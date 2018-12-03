bd943adcb9b6da0457a419f994abcdd22ab10c20
SHS <SHS@macbook-pro-de-shs.local>
2018-12-03 06:06:37 +0100

    Initial Commit

---
+++ Permanent Data Storage/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Permanent Data Storage
+//
+//  Created by SHS on 3/12/18.
+//  Copyright © 2018 SHS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
